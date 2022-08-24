8924b3ffcf8907251c05d7a28083e179d16d04db
tvandinther <tvandinther@gmail.com>
2022-08-24 18:43:19 +1200

    parallelised but kinda slow
    need to optimise memory allocation

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,46 @@
 module Main where
 
 import Lib (
-    WordSet,
+    -- WordSet,
+    WordsMap,
+    WordsMapPair,
+    WordDetail,
     noRepeatLetters,
     wordSet,
     attachBitSet,
     mergeAnagrams,
     mergeUniqueSets,
+    wordsMapPair,
     )
-import Data.Map (Map, toList)
-import Data.List (foldl', nub)
+import Data.Map (Map, toList, union, map)
+import Data.List (foldl')
 import Data.Maybe (isJust)
+import qualified Data.Map as Map
+import Control.Parallel.Strategies (parListChunk, rdeepseq, using, rseq, withStrategy)
+
+-- main = putStrLn "Hello World"
 
 main :: IO ()
 main = do
     words <- lines <$> readFile "words_alpha.txt"
-    let wordSets = allWordSets benchmarkWords
-    let results = iterate (processAllWordSets wordSets) wordSets !! 4
-    print results
+    let wordDetails = toList $ allWordSets words
+    let process = processAllWords wordDetails
+    let processInParallel = processAllWords (wordDetails `using` parListChunk 100 rdeepseq)
+    let results = iterate processInParallel (Prelude.map wordsMapPair wordDetails `using` parListChunk 1000 rdeepseq) !! 4
+    print $ length results
 
 benchmarkWords :: [String]
 benchmarkWords = ["fjord", "gucks", "nymph", "vibex", "waltz"]
 
-processAllWordSets :: [WordSet] -> [WordSet] -> [WordSet]
-processAllWordSets iws rws = foldl' (\acc w1 -> acc ++ processWordSet w1 rws) [] iws
+processAllWords :: [WordDetail] -> [WordsMapPair] -> [WordsMapPair]
+processAllWords iws rws = toList $ foldl' (\acc w -> acc `Map.union` processWordSet w rws) Map.empty iws
 
-allWordSets :: [String] -> [WordSet]
-allWordSets ws = map wordSet $ toList $ mergeAnagrams $ map attachBitSet $ filter noRepeatLetters $ filter (\w -> length w == 5) ws
+-- allWordSets :: [String] -> WordsMap
+allWordSets :: [String] -> Map Int [String]
+allWordSets ws = mergeAnagrams $ Prelude.map attachBitSet $ filter noRepeatLetters $ filter ((== 5) . length) ws
 
-processWordSet :: WordSet -> [WordSet] -> [WordSet]
-processWordSet w1 = foldl' (\acc w2 -> maybe acc (: acc) $ mergeUniqueSets w1 w2) []
+processWordSet :: WordDetail -> [WordsMapPair] -> WordsMap
+processWordSet wd = foldl' (\acc w -> maybe acc (insertWordsMapPairInto acc) $ mergeUniqueSets w wd) Map.empty
+
+insertWordsMapPairInto :: Ord k => Map k a -> (k, a) -> Map k a
+insertWordsMapPairInto m (k,v) = Map.insert k v m
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,12 @@
 module Lib
     (
-        WordSet,
+        -- WordSet,
+        WordsMap,
+        WordsMapPair,
+        WordDetail,
         attachBitSet,
         wordSet,
+        wordsMapPair,
         mergeUniqueSets,
         noRepeatLetters,
         mergeAnagrams,
@@ -12,9 +16,14 @@ import Data.Char ( ord, toLower )
 import Data.Bits ( Bits((.|.), shiftL, bit, (.&.), zeroBits) )
 import Data.Maybe (isJust)
 import Data.Map (fromListWith, Map)
+import qualified Data.Map as Map
 
 type Alpha = Char
 type Word = [Alpha]
+
+type WordsMap = Map Int [[String]]
+type WordsMapPair = (Int, [[String]])
+type WordDetail = (Int, [String])
 
 data WordSet = WordSet {
     words' :: [[String]],
@@ -36,18 +45,18 @@ wordSet (b, ws) = WordSet {
     bitSet = b
 }
 
+wordsMapPair :: WordDetail -> WordsMapPair
+wordsMapPair (b, ws) = (b, [ws])
+
 mergeAnagrams :: (Bits a, Ord a) => [(a, String)] -> Map a [String]
 mergeAnagrams xs = fromListWith (++) [(b, [s]) | (b, s) <- xs]
 
 isUnique :: Bits a => a -> a -> Bool
 isUnique a b = (==) zeroBits $ (.&.) a b
 
-mergeUniqueSets :: WordSet -> WordSet -> Maybe WordSet
-mergeUniqueSets a b
-    | isUnique (bitSet a) (bitSet b) = Just $ WordSet {
-        words' = words' a ++ words' b,
-        bitSet = bitSet a .|. bitSet b
-    }
+mergeUniqueSets :: WordsMapPair -> WordDetail -> Maybe WordsMapPair
+mergeUniqueSets (b1, ws1) (b2, ws2)
+    | isUnique b1 b2 = Just (b1 .|. b2, ws2 : ws1)
     | otherwise = Nothing
 
 noRepeatLetters :: [Alpha] -> Bool
